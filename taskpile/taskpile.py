0b51932066dacc13169e898adddaace304239f7d
Jan Gosmann <jan@hyper-world.de>
2013-07-18 17:54:04 +0200

    Allow to enqueue functions as tasks.

--- taskpile/taskpile.py
+++ taskpile/taskpile.py
@@ -1,5 +1,14 @@
 
 
+class Task(object):
+    def __init__(self, function):
+        self.function = function
+        self.state = 'RUNNING'
+
+
 class Taskpile(object):
     def __init__(self):
         self.queue = []
+
+    def enqueue(self, task_function):
+        self.queue.append(Task(task_function))
--- taskpile/test/test_taskpile.py
+++ taskpile/test/test_taskpile.py
@@ -1,11 +1,33 @@
-from hamcrest import assert_that, is_
+from multiprocessing import Pipe
+
+from hamcrest import assert_that, contains, has_property, is_
 
 from matcher import empty
 
 from taskpile import Taskpile
 
 
+class DummyTask(object):
+    def __init__(self):
+        self.__parent_pipe, self.__child_pipe = Pipe()
+
+    def __call__(self):
+        self.__child_pipe.recv()
+        self.__child_pipe.close()
+
+    def finish(self):
+        self.__parent_pipe.send('arbitrary data to cause exit')
+        self.__parent_pipe.close()
+
+
 class TestTaskpile(object):
-    def test_job_queue_is_initially_empty(self):
-        taskpile = Taskpile()
-        assert_that(taskpile.queue, is_(empty()))
+    def setUp(self):
+        self.taskpile = Taskpile()
+
+    def test_task_queue_is_initially_empty(self):
+        assert_that(self.taskpile.queue, is_(empty()))
+
+    def test_can_add_task_which_will_run_immediatly(self):
+        self.taskpile.enqueue(DummyTask())
+        assert_that(
+            self.taskpile.queue, contains(has_property('state', 'RUNNING')))
