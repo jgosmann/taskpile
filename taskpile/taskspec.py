32edeab0b29254110210299c3331996a774cb153
Jan Gosmann <jan@hyper-world.de>
2013-08-27 15:50:27 +0200

    Task spec instantiation with subsections.

--- taskpile/taskspec.py
+++ taskpile/taskspec.py
@@ -4,17 +4,33 @@ from configobj import ConfigObj
 
 
 class TaskGroupSpec(object):
-    SPEC_KEY = '__spec__'
-    CMD_KEY = 'cmd'
+    CMD_KEY = '__cmd__'
 
     def __init__(self, group_spec):
         self.group_spec = group_spec
 
     @classmethod
     def from_spec_str(cls, spec_str):
-        return cls(ConfigObj(StringIO(spec_str)))
+        return cls(ConfigObj(StringIO(spec_str), interpolation=False))
 
     def iter_specs(self):
-        task_spec = self.group_spec.copy()
-        task_spec[self.SPEC_KEY][self.CMD_KEY] %= self.group_spec
-        yield task_spec
+        for spec in self._iter_subspecs(self.group_spec):
+            spec[self.CMD_KEY] %= spec
+            yield spec
+
+    def _iter_subspecs(self, spec):
+        base_spec = {}
+        spec_gens = []
+        for k, v in spec.items():
+            if hasattr(v, 'items'):
+                spec_gens.append(v)
+            else:
+                base_spec[k] = v
+        if len(spec_gens) <= 0:
+            yield base_spec
+        else:
+            for gen in spec_gens:
+                for spec in self._iter_subspecs(gen):
+                    merged = base_spec.copy()
+                    merged.update(spec)
+                    yield merged
--- taskpile/test/test_taskspec.py
+++ taskpile/test/test_taskspec.py
@@ -1,4 +1,4 @@
-from hamcrest import assert_that, contains
+from hamcrest import assert_that, contains, contains_inanyorder
 from taskpile.taskspec import TaskGroupSpec
 
 
@@ -6,11 +6,35 @@ class TestTaskGroupSpec(object):
     def test_replaces_spec_vars_in_cmd_spec(self):
         spec_str = '''
             somekey = somevalue
-
-            [__spec__]
-                cmd = somecmd --somekey %(somekey)r
+            __cmd__ = somecmd --somekey %(somekey)r
             '''
         group = TaskGroupSpec.from_spec_str(spec_str)
-        expected = {'somekey': 'somevalue', '__spec__': {
-            'cmd': "somecmd --somekey 'somevalue'"}}
+        expected = {
+            'somekey': 'somevalue',
+            '__cmd__': "somecmd --somekey 'somevalue'"}
         assert_that(group.iter_specs(), contains(expected))
+
+    def test_can_use_sections_to_instantiate_task_specs(self):
+        spec_str = '''
+            __cmd__ = %(task_id)s %(sub)s
+            same4all = same
+
+            [task 0]
+                task_id = 0
+                [[subtask0-0]]
+                    sub = 0
+                [[subtaskX-1]]
+                    task_id = X
+                    sub = 1
+
+            [task 1]
+                task_id = 1
+                sub = X
+            '''
+        group = TaskGroupSpec.from_spec_str(spec_str)
+        expected = [
+            {'same4all': 'same', 'task_id': '0', 'sub': '0', '__cmd__': '0 0'},
+            {'same4all': 'same', 'task_id': 'X', 'sub': '1', '__cmd__': 'X 1'},
+            {'same4all': 'same', 'task_id': '1', 'sub': 'X', '__cmd__': '1 X'}
+        ]
+        assert_that(group.iter_specs(), contains_inanyorder(*expected))
